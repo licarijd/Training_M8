05c12f760af79cebd7a6b7c52b7a235e5b8ad1bf
licarijd <licarijd@mcmaster.ca>
2015-05-02 16:33:18 -0400

    initial menu interface

--- app/src/main/java/com/licarijd/jnj/trainingm8/SecondIntroPage.java
+++ app/src/main/java/com/licarijd/jnj/trainingm8/SecondIntroPage.java
@@ -14,7 +14,7 @@ public class SecondIntroPage extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second_page);
     }
 
 
