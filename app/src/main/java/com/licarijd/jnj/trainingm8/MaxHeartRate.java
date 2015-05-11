05265d790a21fce678447d30f3af88e56b6840a8
licarijd <licarijd@mcmaster.ca>
2015-05-11 16:32:51 -0400

    mhr output

--- app/src/main/java/com/licarijd/jnj/trainingm8/MaxHeartRate.java
+++ app/src/main/java/com/licarijd/jnj/trainingm8/MaxHeartRate.java
@@ -6,15 +6,37 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 
 public class MaxHeartRate extends ActionBarActivity {
+
+    private ImageButton calculateMaxHeartRate;
+    private EditText ageInput, resultWin;
+    private RadioButton male, female;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maxheartrate);
+
+        //The text field where the user enters age.
+        ageInput = (EditText) findViewById((R.id.input_age));
+
+        //The calculate button.
+        calculateMaxHeartRate = (ImageButton) findViewById(R.id.calculate_max_heart_rate);
+
+        //The gender selection.
+        male = (RadioButton) findViewById(R.id.male);
+        female = (RadioButton) findViewById(R.id.male);
+
+        //The results window.
+        resultWin = (EditText) findViewById((R.id.mhr_results_window));
+
     }
 
 
@@ -23,6 +45,8 @@ public class MaxHeartRate extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
+
     }
 
     @Override
@@ -49,4 +73,49 @@ public class MaxHeartRate extends ActionBarActivity {
 
     }
 
+
+    public void calculate(View view) {
+
+        //Age is assigned to a String and converted to an integer.
+        String usersAgeText = String.valueOf(ageInput.getText());
+        int usersAge = Integer.parseInt(usersAgeText);
+
+        Boolean isMale = male.isChecked();
+
+        Boolean isFemale = male.isChecked();
+
+        int max = 0;
+
+       if (isMale){
+           max = 220;
+       }
+/*
+        //maximum heart rate for either genders initialized to 0.
+        int max = 0;
+/*
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.male:
+                if (checked)
+
+                    max = 220;
+                    break;
+
+            case R.id.female:
+                if (checked)
+
+                    max = 226;
+                    break;
+        }*/
+
+        int maxHeartRate = max - usersAge;
+
+
+        resultWin.setText("Your Maximum heart rate is " + maxHeartRate);
+    }
+
 }
+
+
+
+
